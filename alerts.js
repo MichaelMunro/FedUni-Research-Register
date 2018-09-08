fd5d073b4519bf8467f1d32099b8238d0b39c221
rohit-randhawa <37392119+rohit-randhawa@users.noreply.github.com>
2018-09-08 19:31:08 +1000

    Add files via upload

---
+++ alerts.js
@@ -0,0 +1,92 @@
+    function show_alert() 
+    {
+        if(confirm("Do you really want to do this?"))
+        {
+            var from = document.getElementById("froms");
+            from.submit();
+        }
+        else
+            return false;
+    }
+function valid()
+{
+var p=document.getElementById("pass1");
+if((p.value.length)<8)
+{
+alert("minimum length of password should be 8");
+return false;
+}
+if ((p.value.length)>15)
+{
+alert("maximum length of password should be 15");
+return false;
+}
+}
+            var conf = document.getElementById("pass2");
+        var conf1 = document.getElementById("pass1");
+        conf.addEventListener("keyup",function(ev)
+        {
+            var sub = document.getElementById('sub');
+            if(document.getElementById('pass1').value==document.getElementById('pass2').value)
+            {
+
+                document.getElementById('message').style.color = 'green';
+                document.getElementById('message').innerHTML = 'matching';
+                sub.style.display="block";
+
+
+            }
+            else
+            {
+                document.getElementById('message').style.color = 'red';
+                document.getElementById('message').innerHTML = 'not matching';
+                sub.style.display="none";
+
+
+            }
+        },false);
+        conf1.addEventListener("keyup",function(ev)
+        {
+            var sub = document.getElementById('sub');
+            if(document.getElementById('pass1').value==document.getElementById('pass2').value)
+            {
+
+                document.getElementById('message').style.color = 'green';
+                document.getElementById('message').innerHTML = 'matching';
+                sub.style.display="block";
+
+
+            }
+            else
+            {
+                document.getElementById('message').style.color = 'red';
+                document.getElementById('message').innerHTML = 'not matching';
+                sub.style.display="none";
+
+
+            }
+        },false);
+ 
+
+
+         
+function degree_alert()
+ 
+{
+   
+   
+ 
+document.getElementById('sub').disabled
+
+=false;
+
+}
+
+
+function degree_alert1()
+{
+document.getElementById('sub').disabled
+
+=true;
+alert('you need a bachelor degree to proceed the registration');
+}
